00a88f86dba7c176ea580c2204222d87f9c61eaf
Yusuf Fakhreddin <48655310+Yusuf-Fakhreddin@users.noreply.github.com>
2022-05-06 01:16:10 +0200

    duaa array

--- cronJobs.js
+++ cronJobs.js
@@ -1,10 +1,5 @@
 const CronJob = require("cron").CronJob;
-const {
-	SalaTweet,
-	ManarTweet,
-	IstghfarTweet,
-	Ist3azaTweet,
-} = require("./tweets");
+const { SalaTweet, ManarTweet, duaaTweet } = require("./tweets");
 
 // cron job every friday at midnight egypt time
 export const SaluAlaElnabi = new CronJob(
@@ -24,17 +19,9 @@ export const Manar = new CronJob(
 	"Africa/Cairo"
 );
 
-export const Ist3aza = new CronJob(
-	"0 10 * * 1,3",
-	Ist3azaTweet,
-	null,
-	false,
-	"Africa/Cairo"
-);
-
-export const Istghfar = new CronJob(
-	"0 10 * * 0,2,4",
-	IstghfarTweet,
+export const duaa = new CronJob(
+	"0 0 * * 1,2,3,4,6,0",
+	duaaTweet,
 	null,
 	false,
 	"Africa/Cairo"
