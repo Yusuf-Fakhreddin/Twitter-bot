6e59b851a7093bf1c89a17d6eed228a08553d436
Yusuf Fakhreddin <48655310+Yusuf-Fakhreddin@users.noreply.github.com>
2022-05-06 01:18:16 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 // import cron jobs
-import { SaluAlaElnabi, Manar, Istghfar, Ist3aza } from "./cronJobs";
+import { SaluAlaElnabi, Manar, duaa } from "./cronJobs";
 
 SaluAlaElnabi.start();
 Manar.start();
-Istghfar.start();
-Ist3aza.start();
+duaa.start();
+
+export const test = async () => {
+	const tweet = {
+		status: "test",
+	};
+
+	try {
+		const result = await T.post("statuses/update", tweet);
+		console.log("test");
+	} catch (err) {
+		console.log(err);
+	}
+};
+
+test();
