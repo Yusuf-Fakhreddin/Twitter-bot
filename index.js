52f7b3b922ac49f83495e460550d626da8387fba
Yusuf Fakhreddin <48655310+Yusuf-Fakhreddin@users.noreply.github.com>
2022-05-08 03:10:47 +0200

    commented test func

--- index.js
+++ index.js
@@ -6,17 +6,17 @@ SaluAlaElnabi.start();
 Manar.start();
 duaa.start();
 
-const test = async () => {
-	const tweet = {
-		status: "test",
-	};
+// const test = async () => {
+// 	const tweet = {
+// 		status: "test",
+// 	};
 
-	try {
-		const result = await T.post("statuses/update", tweet);
-		console.log("test");
-	} catch (err) {
-		console.log(err);
-	}
-};
+// 	try {
+// 		const result = await T.post("statuses/update", tweet);
+// 		console.log("test");
+// 	} catch (err) {
+// 		console.log(err);
+// 	}
+// };
 
-test();
+// test();
